8140c1dcfc30952f1b947c92c653da0e72e184a2
Sandhya <sandhyarana07610761@gmail.com>
2025-07-08 21:11:18 +0530

    first commit

---
+++ Reduxs/src/app/store.js
@@ -0,0 +1,15 @@
+// src/app/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import userReducer from '../features/user/userSlice';
+import { postsApi } from '../features/posts/postsApiSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+    [postsApi.reducerPath]: postsApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postsApi.middleware),
+});
---
+++ Reduxs/src/features/auth/authSlice.js
@@ -0,0 +1,20 @@
+// src/features/auth/authSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { isLoggedIn: false, userId: null },
+  reducers: {
+    login: (state, action) => {
+      state.isLoggedIn = true;
+      state.userId = action.payload;
+    },
+    logout: (state) => {
+      state.isLoggedIn = false;
+      state.userId = null;
+    }
+  }
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
